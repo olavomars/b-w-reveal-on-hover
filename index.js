e9e5332fa2ea96b5a3cc12f83766e26bb83a368d
Olavo Mars <olavoamneves@gmail.com>
2024-01-08 19:20:28 -0400

    cleaning up

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ document.addEventListener('mousemove', (e) => {
   cursor.style.left = e.pageX + 'px';
   cursor.style.top = e.pageY + 'px';
 
-  if (isOverlaping(cursor, first)) {
+  if (isOverlaping(cursor, secondText)) {
+    first.style.display = 'flex';
+    cursor.style.transform = 'scale(12)';
     const cursorRect = cursor.getBoundingClientRect();
     let cursorWidth = cursorRect.width / 2;
     let cursorX = cursorRect.x;
@@ -28,11 +30,6 @@ document.addEventListener('mousemove', (e) => {
 
     first.style.maskPosition = `${cursorX}px ${cursorY}px`;
     first.style.maskSize = `${cursorWidth * 2}px`;
-  }
-  if (isOverlaping(cursor, secondText)) {
-    first.style.display = 'flex';
-    cursor.style.transform = 'scale(12)';
-    cursor.style.background = '#fff';
   } else {
     first.style.display = 'none';
     cursor.style.transform = 'scale(1)';
