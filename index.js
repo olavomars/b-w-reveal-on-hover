2df853a3815832739ad3c7d4ebd5df24fb1daf63
Olavo Mars <olavoamneves@gmail.com>
2024-01-08 19:12:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let cursor = document.querySelector('.cursor');
+let textContainer = document.querySelector('.text-container');
+let first = document.querySelector('#first');
+let second = document.querySelector('#second');
+let secondText = document.querySelector('#second h2');
+
+const isOverlaping = (a, b) => {
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
+
+  return !(
+    aRect.top > bRect.bottom ||
+    aRect.bottom < bRect.top ||
+    aRect.right < bRect.left ||
+    aRect.left > bRect.right
+  );
+};
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.pageX + 'px';
+  cursor.style.top = e.pageY + 'px';
+
+  if (isOverlaping(cursor, first)) {
+    const cursorRect = cursor.getBoundingClientRect();
+    let cursorWidth = cursorRect.width / 2;
+    let cursorX = cursorRect.x;
+    let cursorY = cursorRect.y;
+
+    first.style.maskPosition = `${cursorX}px ${cursorY}px`;
+    first.style.maskSize = `${cursorWidth * 2}px`;
+  }
+  if (isOverlaping(cursor, secondText)) {
+    first.style.display = 'flex';
+    cursor.style.transform = 'scale(12)';
+    cursor.style.background = '#fff';
+  } else {
+    first.style.display = 'none';
+    cursor.style.transform = 'scale(1)';
+  }
+});
